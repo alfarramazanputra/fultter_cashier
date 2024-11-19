2ebe59d55415a468699f0e4b59f6641af2ad4183
alfarramazanputra <alfarbogor@gmail.com>
2024-11-19 10:37:16 +0700

    flutter cashier

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:cashier_app/cashierPage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Cashier App',
+      home: const CashierPage(),
+    );
+  }
+}
